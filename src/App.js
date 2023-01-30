3a09f8be86487416289a7d21c2756ffd1d5d0561
AlemkhanIT <captainkenpachi7@gmail.com>
2023-01-30 21:28:28 +0100

    Axios (Delete,Add to cart), added Search, update scss

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React,{useEffect, useState} from "react";
 import Cards from "./components/Cards/Cards";
+import axios from "axios";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
  function App() {
   const [items,setItems]=useState([]);
   const [itemsCart,setItemsCart]=useState([]);
   const [isCarted, setIsCarted]=useState(false);
+  const [search,setSearch] = useState('');
   useEffect(()=>{
-    fetch('https://63d6c3a7dc3c55baf43c1957.mockapi.io/items').then((res)=>{
-      return res.json();
-    }).then((json)=>{
-      setItems(json);
+    axios.get('https://63d6c3a7dc3c55baf43c1957.mockapi.io/items').then((res)=>{
+      setItems(res.data)
+    })
+    axios.get('https://63d6c3a7dc3c55baf43c1957.mockapi.io/cart').then((res)=>{
+      setItemsCart(res.data)
     })
   },[]);
 
@@ -18,30 +21,39 @@ import Header from "./components/Header";
     console.log(obj)
   }
   const onAddToCart = (obj) =>{
+    axios.post('https://63d6c3a7dc3c55baf43c1957.mockapi.io/cart', obj)
     setItemsCart(prev=>[...prev,obj]);
   }
-  
+  const onRemoveItem = (id) =>{
+    console.log(id)
+    axios.delete(`https://63d6c3a7dc3c55baf43c1957.mockapi.io/cart/${id}`)
+    setItemsCart(prev=>prev.filter(item=>item.id!==id));
+  }
+  const onChangeSearch = (event) =>{
+    setSearch(event.target.value)
+  }
   
   return (
     <div className="wrapper">
       {isCarted?<div className="overlay">
-        <Drawer items={itemsCart} onClose={()=>setIsCarted(!isCarted)} />
+        <Drawer items={itemsCart} onClose={()=>setIsCarted(!isCarted)} onRemove={onRemoveItem}/>
       </div>:null}
     <Header 
       onClickCart={()=>setIsCarted(!isCarted)}
     />
     <div className="content">
       <div className="on__content">
-        <h1>The All Quadrocopters</h1>
+        <h1>{search?`Search on: "${search}"`:"The All Quadrocopters"}</h1>
         <label className="search">
           <img src="/img/search.svg"/>
-          <input className="search__input" type="text" placeholder="Search..." />
+          <input onChange={onChangeSearch} className="search__input" type="text" placeholder="Search..." />
         </label>
       </div>
       <div className="items">
         
-          {items.map((item)=>(
+          {items.filter((item)=>item.title.toLowerCase().includes(search)).map((item,index)=>(
             <Cards 
+            key={index}
             title={item.title}
             price={item.price}
             imgUrl={item.imgUrl}
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,39 +1,45 @@
 import React from 'react'
 
-function Drawer({onClose, items=[]}) {
+function Drawer({onClose, items=[],onRemove}) {
   
   return (
     <div className="drawer">
           <div className="drawer__top"><h3>Cart</h3> <div onClick={onClose} className='button-close'><img src="/img/nonactive_cart.svg"/></div></div>
-          <div className="drawer__cost">
-          <div className="drawer__items">
-            {items.map((item)=>(
-              <div className="drawer__card">
-              <img src={item.imgUrl}/>
-              <div className="drawer__info">
-                <p>{item.title}</p>
-                <span>{item.price} dlr.</span>
+          {items.length==0?(
+            <div className='drawer__empty'><p>Cart is empty</p><br/>
+            <img height={340} src='img/box.webp'/>
+            <button onClick={onClose} className='buttonKl'>Go back to add to cart <img src="img/strela.svg" /></button>
+            </div>
+          ):(
+            <div className="drawer__cost">
+            <div className="drawer__items">
+              {items.map((item)=>(
+                <div className="drawer__card">
+                <img src={item.imgUrl}/>
+                <div className="drawer__info">
+                  <p>{item.title}</p>
+                  <span>{item.price} dlr.</span>
+                </div>
+                <div onClick={()=>onRemove(item.id)} className='btnClose'><img src="/img/nonactive_cart.svg"/></div>
               </div>
-              <button><img src="/img/nonactive_cart.svg"/></button>
+              ))}
             </div>
-            ))}
-            
-            
-          </div>
-          <div className="drawer__price">
-            <div className="drawer__final">
-              <p>Final price: </p>
-              <div></div>
-              <b>2410 dlr.</b>
+            <div className="drawer__price">
+              <div className="drawer__final">
+                <p>Final price: </p>
+                <div></div>
+                <b>2410 dlr.</b>
+              </div>
+              <div className="drawer__final">
+                <p>Tax 5%: </p>
+                <div></div>
+                <b>60 dlr.</b>
+              </div>
+              <button className='buttonKl'>Buy <img src="img/strela.svg" /></button>
             </div>
-            <div className="drawer__final">
-              <p>Tax 5%: </p>
-              <div></div>
-              <b>60 dlr.</b>
             </div>
-            <button>Buy <img src="img/strela.svg" /></button>
-          </div>
-          </div>
+          )}
+
         </div>
   )
 }
